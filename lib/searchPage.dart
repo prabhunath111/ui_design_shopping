8df3e4412909772b9e8e592d296a481047915af2
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-03-20 21:11:12 +0530

    added image in horizontal listview in search page

--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -13,9 +13,15 @@ class _SearchState extends State<Search> {
 
   @override
   Widget build(BuildContext context) {
-    double hsfs = MediaQuery.of(context).size.height;
+    double hsfs = MediaQuery
+        .of(context)
+        .size
+        .height;
 
-    double wsfs = MediaQuery.of(context).size.width;
+    double wsfs = MediaQuery
+        .of(context)
+        .size
+        .width;
 
     return Scaffold(
       body: Stack(
@@ -41,13 +47,13 @@ class _SearchState extends State<Search> {
                         boxShadow: [
                           BoxShadow(
                             color: Colors.grey,
-                            blurRadius: 15.0,
+                            blurRadius: 1.0,
                             // has the effect of softening the shadow
                             spreadRadius: 0.0,
                             // has the effect of extending the shadow
                             offset: Offset(
                               5.0, // horizontal, move right 10
-                              10.0, // vertical, move down 10
+                              5.0, // vertical, move down 10
                             ),
                           )
                         ],
@@ -67,16 +73,22 @@ class _SearchState extends State<Search> {
                   ],
                 ),
               ),
-              SizedBox(),
+              SizedBox(height: hsfs * 0.01,),
               Container(
                 margin: EdgeInsets.only(left: 16.0, top: 16.0),
-                height: MediaQuery.of(context).size.height * 0.1,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.18,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: <Widget>[
                     Container(
                       margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      width: MediaQuery.of(context).size.width * 0.6,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.6,
                       child: Column(
                         children: <Widget>[
                           Text(
@@ -86,56 +98,72 @@ class _SearchState extends State<Search> {
                                 fontWeight: FontWeight.bold,
                                 fontSize: 14.0),
                           ),
-                          Card(
-                            elevation: 1.0,
-                            child: Wrap(
-                              children: <Widget>[
-                                Column(
-                                  children: <Widget>[
-                                    Container(
-                                      child: Row(
-                                        children: <Widget>[
-                                          Container(
-                                            width: 30.0,
-                                            height: 30.0,
-                                            decoration: BoxDecoration(
-                                              image: DecorationImage(
-                                                fit: BoxFit.contain,
-                                                image: ExactAssetImage(
-                                                    'images/shoe.jpg'),
-                                              ),
-                                            ),
-                                          ),
-                                          Column(
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Card(
+                              elevation: 1.0,
+                              child: Wrap(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 0.0,
+                                              top: 8.0,
+                                              right: 4.0,
+                                              bottom: 8.0),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment
+                                                .spaceBetween,
                                             children: <Widget>[
-                                              Text(
-                                                'Nike Air Max',
-                                                style: TextStyle(
-                                                    color: Colors.grey,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 18.0),
+                                              Container(
+                                                width: 65.0,
+                                                height: 30.0,
+                                                decoration: BoxDecoration(
+                                                  image: DecorationImage(
+                                                    fit: BoxFit.contain,
+                                                    image: ExactAssetImage(
+                                                        'images/shoe.jpg'),
+                                                  ),
+                                                ),
                                               ),
-                                              Text('14:13:22:50',
-                                                  style: TextStyle(
-                                                      color: Colors.grey,
-                                                      fontWeight:
+                                              Column(
+                                                children: <Widget>[
+                                                  Text(
+                                                    'Nike Air Max',
+                                                    style: TextStyle(
+                                                        color: Colors.grey,
+                                                        fontWeight: FontWeight
+                                                            .bold,
+                                                        fontSize: 18.0),
+                                                  ),
+                                                  Text('14:13:22:50',
+                                                      style: TextStyle(
+                                                          color: Colors.grey,
+                                                          fontWeight:
                                                           FontWeight.bold)),
+                                                ],
+                                              )
                                             ],
-                                          )
-                                        ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      width: MediaQuery.of(context).size.width * 0.6,
+                      margin: EdgeInsets.symmetric(horizontal: 2.0),
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.6,
                       child: Column(
                         children: <Widget>[
                           Text(
@@ -145,50 +173,65 @@ class _SearchState extends State<Search> {
                                 fontWeight: FontWeight.bold,
                                 fontSize: 14.0),
                           ),
-                          Card(
-                            elevation: 1.0,
-                            child: Wrap(
-                              children: <Widget>[
-                                Column(
-                                  children: <Widget>[
-                                    Container(
-                                      child: Row(
-                                        children: <Widget>[
-                                          Container(
-                                            width: 30.0,
-                                            height: 30.0,
-                                            decoration: BoxDecoration(
-                                              image: DecorationImage(
-                                                fit: BoxFit.contain,
-                                                image: ExactAssetImage(
-                                                    'images/shoe2.jpeg'),
-                                              ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Card(
+                              elevation: 1.0,
+                              child: Wrap(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        child: Container(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 0.0,
+                                                top: 8.0,
+                                                right: 4.0,
+                                                bottom: 8.0),
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment
+                                                  .spaceBetween,
+                                              children: <Widget>[
+                                                Container(
+                                                  width: 65.0,
+                                                  height: 30.0,
+                                                  decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                      fit: BoxFit.contain,
+                                                      image: ExactAssetImage(
+                                                          'images/shoe2.jpeg'),
+                                                    ),
+                                                  ),
+                                                ),
+                                                Column(
+                                                  crossAxisAlignment:
+                                                  CrossAxisAlignment.end,
+                                                  children: <Widget>[
+                                                    Text(
+                                                      'Nike Air Max',
+                                                      style: TextStyle(
+                                                          color: Colors.grey,
+                                                          fontWeight: FontWeight
+                                                              .bold,
+                                                          fontSize: 18.0),
+                                                    ),
+                                                    Text('14:13:22:50',
+                                                        style: TextStyle(
+                                                            color: Colors.grey,
+                                                            fontWeight:
+                                                            FontWeight.bold)),
+                                                  ],
+                                                )
+                                              ],
                                             ),
                                           ),
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.end,
-                                            children: <Widget>[
-                                              Text(
-                                                'Nike Air Max',
-                                                style: TextStyle(
-                                                    color: Colors.grey,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 18.0),
-                                              ),
-                                              Text('14:13:22:50',
-                                                  style: TextStyle(
-                                                      color: Colors.grey,
-                                                      fontWeight:
-                                                          FontWeight.bold)),
-                                            ],
-                                          )
-                                        ],
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
@@ -198,7 +241,40 @@ class _SearchState extends State<Search> {
                 ),
               ),
               SizedBox(
-                height: 40.0,
+                height: 0.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16.0, top: 8.0, bottom: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('People', style: TextStyle(color: Colors.grey)),
+                        Container(
+                            margin: EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Text('CLEAR', style: TextStyle(
+                                color: customColorWithoutGradient),)),
+                      ],
+                    ),
+
+                    SizedBox(height: hsfs * 0.015),
+
+                    Container(
+                      height: hsfs * 0.25,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          _peopleList(),_peopleList(),_peopleList(),_peopleList(),_peopleList(),_peopleList(),
+
+                        ],
+                      ),
+                    )
+
+                  ],
+                ),
               ),
             ],
           ),
@@ -256,5 +332,29 @@ class _SearchState extends State<Search> {
         ],
       ),
     );
+  }
+
+  Widget _peopleList() {
+    return
+      Wrap(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.21,
+            child:
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+                width: 100.0,
+                height: 70.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: ExactAssetImage(
+                          'images/user.jpg'),
+                    ))),
+
+          ),
+        ],
+      );
   }
 }
